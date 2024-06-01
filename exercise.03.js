31833e05f418c10119e5fe6e6d495d168c50e130
joabe <joabe9999pereira@gmail.com>
2024-06-01 13:35:53 -0300

    exercise03

---
+++ exercise.03.js
@@ -0,0 +1,22 @@
+// Compare two arrays and return a new array with items found only in one of the two given arrays,
+// but not in both. In other words, return the symmetric difference of the two arrays.
+// Note: You can return the array with its elements in any order.
+
+function diffArray(arr1, arr2) {
+  const newArr = [];
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr2.indexOf(arr1[i]) === -1) {
+      newArr.push(arr1[i]);
+    }
+  }
+
+  for (let j = 0; j < arr2.length; j++) {
+    if (arr1.indexOf(arr2[j]) === -1 && newArr.indexOf(arr2[j]) === -1) {
+      newArr.push(arr2[j]);
+    }
+  }
+  return newArr;
+}
+
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
